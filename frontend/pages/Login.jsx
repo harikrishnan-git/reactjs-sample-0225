1d86b4c99eb5ba9dc7d2c5881cfebdd5e6427bea
harikrishnan-git <haribkrishh@gmail.com>
2025-06-05 15:08:10 +0530

    small update

--- frontend/pages/Login.jsx
+++ frontend/pages/Login.jsx
@@ -64,26 +64,12 @@ export default function Login() {
           />
         </div>
         <div className="flex items-start mb-5">
-          <div className="flex items-center h-5">
-            <input
-              id="terms"
-              type="checkbox"
-              value=""
-              className="w-4 h-4 border border-gray-300 rounded-sm bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-            />
-          </div>
-          <div className="flex items-center justify-between w-full">
-            <label
-              htmlFor="terms"
-              className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-            >
-              Remember me
-            </label>
+          <div className="flex items-center justify-end w-full">
             <a
               href="/signup"
-              className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300 hover:underline hover:text-blue-400 "
+              className="ms-2 text-sm font-medium text-blue-400 underline hover:text-blue-600 "
             >
-              Forgot password
+              Not Registered?
             </a>
           </div>
         </div>
