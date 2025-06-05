d6dcdf7ee0a978f644b3ee890d25183021528134
harikrishnan-git <75241396+harikrishnan-git@users.noreply.github.com>
2025-06-05 18:56:55 +0530

    Updated Navbar.jsx

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -25,10 +25,10 @@ export default function Navbar() {
           >
             <img
               src="https://flowbite.com/docs/images/logo.svg"
-              className="h-8"
+              className="hidden h-8"
               alt="Flowbite Logo"
             />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            <span className="hidden self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
               Flowbite
             </span>
           </a>
